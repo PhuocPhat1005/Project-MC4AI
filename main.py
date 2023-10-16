92073358d45bcd414fc6f5946b2b8f8b7abaaa29
Le Phuoc Phat <phatlephuoc1005@gmail.com>
2023-10-16 07:54:19 +0700

    Them main

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+tab1, tab2, tab3, tab4 = st.tabs(["Danh sách", "Biểu đồ", "Phân nhóm", "Phân loại"])
+with tab1:
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+    with col2:
+    with col3:
+    with col4:
+with tab2:
+with tab3:
+with tab4:
