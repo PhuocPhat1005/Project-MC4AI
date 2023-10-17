62142a4dee1eb787ae21837ef6d7d2ee4a29014f
Le Phuoc Phat <phatlephuoc1005@gmail.com>
2023-10-17 17:19:23 +0700

    them

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
+
+df = pd.read_csv('py4ai-score.csv')
+df['S1'].fillna(0, inplace=True)
+df['S2'].fillna(0, inplace=True)
+df['S3'].fillna(0, inplace=True)
+df['S4'].fillna(0, inplace=True)
+df['S5'].fillna(0, inplace=True)
+df['S6'].fillna(0, inplace=True)
+df['S7'].fillna(0, inplace=True)
+df['S8'].fillna(0, inplace=True)
+df['S9'].fillna(0, inplace=True)
+df['S10'].fillna(0, inplace=True)
+df['BONUS'].fillna(0, inplace=True)
+df['REG-MC4AI'].fillna('unknown', inplace=True)
+    
 
 tab1, tab2, tab3, tab4 = st.tabs(["Danh sách", "Biểu đồ", "Phân nhóm", "Phân loại"])
 with tab1:
@@ -9,7 +25,7 @@ with tab1:
         choose1 = st.checkbox('Nam')
         choose2 = st.checkbox('Nữ')
     with col2:
-        genre = st.radio("Khối lớp", ["Tất cả", "Lớp 10", "Lớp 11", "Lớp 12"])
+        radio = st.radio("Khối lớp", ["Tất cả", "Lớp 10", "Lớp 11", "Lớp 12"])
     with col3:
         option_1 = st.selectbox('Phòng', ['Tất cả', 'A114', 'A115'], placeholder = "Choose an option")
     with col4:
@@ -33,6 +49,18 @@ with tab1:
     with col9:
         choose11 = st.checkbox("TH/SN")
         choose12 = st.checkbox("Khác")
+        
+    if choose1 and choose2:
+        st.dataframe(df)
+    elif choose1:
+        df = df[df['GENDER'].str.contains('M')]
+        st.dataframe(df)
+    elif choose2:
+        df = df[df['GENDER'].str.contains('F')]
+        st.dataframe(df)
+
+    if radio == 'Lớp 10':
+    
     
 with tab2:
     tab_A, tab_B = st.tabs(["Số lượng HS", "Điểm"])
