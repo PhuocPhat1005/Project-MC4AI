b1657769bfc1b73b45bb87de2faddbf810c5e59f
Le Phuoc Phat <phatlephuoc1005@gmail.com>
2023-10-20 02:00:47 +0700

    xong bieu do

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 
-
-# Xử lý và loại bỏ giá trị NULL trong dataframe
 df = pd.read_csv('py4ai-score.csv')
 
 df['S1'].fillna(0, inplace=True)
@@ -21,23 +19,23 @@ df['S10'].fillna(0, inplace=True)
 df['BONUS'].fillna(0, inplace=True)
 df['REG-MC4AI'].fillna('unknown', inplace=True)
 
-st.title('BẢNG ĐIỂM LỚP PY4AI 09/2022')
+st.title(':rainbow[BẢNG ĐIỂM LỚP PY4AI 09/2022]')
 
-tab1, tab2, tab3, tab4 = st.tabs(["Danh sách", "Biểu đồ", "Phân nhóm", "Phân loại"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["**Danh sách**", "**Biểu đồ**", "**Phân nhóm**", "**Phân loại**", "**Điểm danh**"])
 with tab1:
     col1, col2, col3, col4 = st.columns(4)
     with col1:
-        st.write("Giới tính")
+        st.write("**Giới tính**")
         choose1 = st.checkbox('Nam')
         choose2 = st.checkbox('Nữ')
     with col2:
-        radio = st.radio("Khối lớp", ["Tất cả", "Lớp 10", "Lớp 11", "Lớp 12"])
+        radio = st.radio("**Khối lớp**", ["Tất cả", "Lớp 10", "Lớp 11", "Lớp 12"])
     with col3:
-        option_1 = st.selectbox('Phòng', ['Tất cả', 'A114', 'A115'], placeholder = "Choose an option")
+        option_1 = st.selectbox('**Phòng**', ['Tất cả', 'A114', 'A115'], placeholder = "Choose an option")
     with col4:
-        option_2 = st.multiselect("Buổi", ["Sáng", "Chiều"], placeholder = "Choose an option")
+        option_2 = st.multiselect("**Buổi**", ["Sáng", "Chiều"], placeholder = "Choose an option")
 
-    st.write("Lớp chuyên")
+    st.write("**Lớp chuyên**")
 
     col5, col6, col7, col8, col9 = st.columns(5)
     with col5:
@@ -133,102 +131,223 @@ with tab1:
     elif num_true == 10:
         indices = [i for i, label in enumerate(labels) if label]
         df = df[filter_conditions[indices[0]] | filter_conditions[indices[1]] | filter_conditions[indices[2]] | filter_conditions[indices[3]] | filter_conditions[indices[4]] | filter_conditions[indices[5]] | filter_conditions[indices[6]] | filter_conditions[indices[7]] | filter_conditions[indices[8]] | filter_conditions[indices[9]]]
-        
-    cnt_1 = 0
-    cnt_2 = 0
-    for i in df.index:
-        c = df.loc[i, 'GENDER']
-        if c == 'M':
-            cnt_1 += 1
-        elif c == 'F':
-            cnt_2 += 1
-            
+    
+    cnt_1 = len(df[df['GENDER'].str.contains('M')])
+    cnt_2 = len(df[df['GENDER'].str.contains('F')])
+    count_reg_Y = len(df[df['REG-MC4AI'] == 'Y'])
+    count_reg_U = len(df[df['REG-MC4AI'] == 'unknown'])
+    
     if df is not None:
         students = len(df)
         maxGPA = df['GPA'].max()
         minGPA = df['GPA'].min()
         averageGPA = round(df['GPA'].mean(), 1)
-        st.write('Số HS:', students, f' ({cnt_1} nam, {cnt_2} nữ)')
-        st.write('GPA: cao nhất', maxGPA, ', thấp nhất ', minGPA, ', trung bình', averageGPA)
-        
+        st.write('**Số HS** :', students, f' ({cnt_1} nam, {cnt_2} nữ)')
+        st.write('**GPA** : cao nhất', maxGPA, ', thấp nhất ', minGPA, ', trung bình', averageGPA)
+        st.write('**Số HS đăng ký tiếp lớp MC-4AI** :', count_reg_Y, ', **không đăng ký tiếp lớp MC4AI** : ', count_reg_U)
     st.dataframe(df)
     
 with tab2:
     
-    tab_A, tab_B = st.tabs(["Số lượng HS", "Điểm"])
+    tab_A, tab_B = st.tabs(["**Số lượng HS**", "**Điểm**"])
     
     with tab_A:
-        count_114S = len(df[df['PYTHON-CLASS'] == '114-S'])
-        count_114C = len(df[df['PYTHON-CLASS'] == '114-C'])
-        count_115S = len(df[df['PYTHON-CLASS'] == '115-S'])
-        count_115C = len(df[df['PYTHON-CLASS'] == '115-C'])
-        
-        chart_1 = [count_114S, count_114C, count_115S, count_115C]
-        name_class = []
-        sizes = []
-        conditions = {
-            0: '114-S',
-            1: '114-C',
-            2: '115-S',
-            3: '115-C'
-        }
-        
-        for i in range(len(chart_1)):
-            if chart_1[i] != 0:
-                name_class.append(conditions[i])
-                sizes.append(chart_1[i])
-        
-        fig, ax = plt.subplots(figsize = (6, 6))
-        ax.pie(sizes, autopct='%1.1f%%', startangle = 90)
-        ax.axis('equal')
-        plt.legend(name_class, prop = {'size': 7}, loc = "upper right")
-        plt.title('BIỂU ĐỒ BIỂU DIỄN SỰ SO SÁNH SỐ LƯỢNG HỌC SINH TỪNG BUỔI HỌC')
-        st.pyplot(fig)
-         
-        conditions_1 = {
-            0: 'Chuyên Văn',
-            1: 'Chuyên Toán',
-            2: 'Chuyên Lý',
-            3: 'Chuyên Hóa',
-            4: 'Chuyên Anh',
-            5: 'Chuyên Tin',
-            6: 'Chuyên Sử Địa',
-            7: 'Chuyên Trung Nhật',
-            8: 'Tích Hợp / Song Ngữ',
-            9: 'Khác'
-        }
+        if(len(df)) != 0:
+            # Draw the pie chart 1
+            count_114S = len(df[df['PYTHON-CLASS'] == '114-S'])
+            count_114C = len(df[df['PYTHON-CLASS'] == '114-C'])
+            count_115S = len(df[df['PYTHON-CLASS'] == '115-S'])
+            count_115C = len(df[df['PYTHON-CLASS'] == '115-C'])
+            
+            chart_1 = [count_114S, count_114C, count_115S, count_115C]
+            name_class = []
+            sizes = []
+            conditions = {
+                0: '114-S', 
+                1: '114-C',
+                2: '115-S',
+                3: '115-C'
+            }
+            
+            for i in range(len(chart_1)):
+                if chart_1[i] != 0:
+                    name_class.append(conditions[i])
+                    sizes.append(chart_1[i])
+            
+            sizes = np.array(sizes)
+            name_class = np.array(name_class)
+            
+            fig = px.pie(df, names = 'PYTHON-CLASS', title = 'BIỂU ĐỒ BIỂU DIỄN SỰ SO SÁNH SỐ LƯỢNG HỌC SINH TỪNG BUỔI HỌC')
+            fig.update_layout(title_x=0.1, title_y=0.9)
+            st.plotly_chart(fig)
 
-        name_class_1 = []
-        sizes_1 = []
+            if len(sizes) == 1:
+                st.success(f'**Kết luận**:\n\n- Chỉ có duy nhất một lớp học **{name_class[0]}** (100%).\n\n- Đây là thời điểm thích hợp nhiều người học hơn.', icon = "✍️")
+            elif len(sizes) == 2:
+                if (sizes[0] < sizes[1]):
+                    st.success(f'**Kết luận**:\n\n- Có hai lớp học: **{name_class[0]}** và **{name_class[1]}** .\n\n- Ta thấy số lượng học sinh lớp **{name_class[0]}** ít hơn số lượng học sinh lớp **{name_class[1]}**.\n\n- Chứng tỏ học sinh có xu hướng học lớp **{name_class[1]}** nhiều hơn.', icon = "✍️")
+                elif (sizes[0] > sizes[1]):
+                    st.success(f'**Kết luận**:\n\n- Có hai lớp học: **{name_class[0]}** và **{name_class[1]}** .\n\n- Ta thấy số lượng học sinh lớp **{name_class[1]}** ít hơn số lượng học sinh lớp **{name_class[0]}**.\n\n- Chứng tỏ học sinh có xu hướng học lớp **{name_class[0]}** nhiều hơn.', icon = "✍️")
+                elif (sizes[0] == sizes[1]):
+                    st.success(f'**Kết luận**:\n\n- Có hai lớp học: **{name_class[0]}** và **{name_class[1]}** .\n\n- Ta thấy số lượng học sinh lớp **{name_class[0]}** bằng số lượng học sinh lớp **{name_class[1]}**.\n\n- Chứng tỏ giờ học là hợp lý, đáp ứng đủ nhu cầu của từng học sinh.', icon = "✍️")
+            elif len(sizes) == 3:
+                max_id = sizes.argmax()
+                min_id = sizes.argmin()
+                st.success(f'**Kết luận**:\n\n- Có ba lớp học: **{name_class[0]}**, **{name_class[1]}** và **{name_class[2]}** .\n\n- Lớp có số lượng học sinh học đông nhất là: **{name_class[max_id]}** ({sizes[max_id]} học sinh).\n\n- Lớp học có số lượng học sinh học ít nhất là: **{name_class[min_id]}** ({sizes[min_id]} học sinh).\n\n- Chứng tỏ học sinh có thời gian học lớp **{name_class[max_id]}** hơn các lớp còn lại, trong khi đó lại có xu hướng ít học lớp **{name_class[min_id]}**. Đồng thời, các lớp còn lại thì có số lưọng bình thường.', icon = "✍️")
+            elif len(sizes) == 4:
+                max_id = sizes.argmax()
+                min_id = sizes.argmin()
+                st.success(f'**Kết luận**:\n\n- Có bốn lớp học: **{name_class[0]}**, **{name_class[1]}**, **{name_class[2]}** và **{name_class[3]}** .\n\n- Lớp có số lượng học sinh học đông nhất là: **{name_class[max_id]}** ({sizes[max_id]} học sinh).\n\n- Lớp học có số lượng học sinh học ít nhất là: **{name_class[min_id]}** ({sizes[min_id]} học sinh).\n\n- Chứng tỏ học sinh có thời gian học lớp **{name_class[max_id]}** hơn các lớp còn lại, trong khi đó lại có xu hướng ít học lớp **{name_class[min_id]}**. Đồng thời, các lớp còn lại thì có số lượng bình thường.', icon = "✍️")
+            
+            # Draw the pie chart 2
+            rank = []
+            for i in df.index:
+                c = df.loc[i, 'CLASS']
+                if c[2:4] == 'CV':
+                    rank.append('Văn')
+                elif c[2:5] == 'CT1' or c[2:5] == 'CT2' or c[2:5] == 'CT3':
+                    rank.append('Toán')
+                elif c[2:4] == 'CL':
+                    rank.append('Lý')
+                elif c[2:4] == 'CH':
+                    rank.append('Hóa')
+                elif c[2:4] == 'CA':
+                    rank.append('Anh')
+                elif c[2:6] == 'CTIN':
+                    rank.append('Tin')
+                elif c[2:5] == 'CSD':
+                    rank.append('Sử Địa')
+                elif c[2:6] == 'CTRN':
+                    rank.append('Trung Nhật')
+                elif c[2:4] == 'TH' or c[2:4] == 'SN':
+                    rank.append('TH / SN')
+                elif c[2] == 'A' or c[2] == 'B':
+                    rank.append('Khác')
+                    
+            df['CLASS-GROUP'] = rank
+            fig = px.pie(df, names = 'CLASS-GROUP', title = 'BIỂU ĐỒ BIỂU DIỄN SỰ SO SÁNH SỐ LƯỢNG HỌC SINH TỪNG LỚP HỌC')
+            fig.update_layout(title_x=0.1, title_y=0.9)
+            st.plotly_chart(fig)
+            
+            conditions_1 = {
+                0: 'Chuyên Văn',
+                1: 'Chuyên Toán',
+                2: 'Chuyên Lý',
+                3: 'Chuyên Hóa',
+                4: 'Chuyên Anh',
+                5: 'Chuyên Tin',
+                6: 'Chuyên Sử Địa',
+                7: 'Chuyên Trung Nhật',
+                8: 'Tích Hợp / Song Ngữ',
+                9: 'Khác'
+            }
 
-        count_0 = len(df[df['CLASS'].str[2:4].isin(['CV'])])
-        count_1 = len(df[df['CLASS'].str[2:5].isin(['CT1', 'CT2', 'CT3'])])
-        count_2 = len(df[df['CLASS'].str[2:4].isin(['CL'])])
-        count_3 = len(df[df['CLASS'].str[2:4].isin(['CH'])])
-        count_4 = len(df[df['CLASS'].str[2:4].isin(['CA'])])
-        count_5 = len(df[df['CLASS'].str[2:6].isin(['CTIN'])])
-        count_6 = len(df[df['CLASS'].str[2:5].isin(['CSD'])])
-        count_7 = len(df[df['CLASS'].str[2:6].isin(['CTRN'])])
-        count_8 = len(df[df['CLASS'].str[2:4].isin(['TH', 'SN'])])
-        count_9 = len(df[df['CLASS'].str[2].isin(['A', 'B'])])
+            name_class_1 = []
+            sizes_1 = []
 
-        for i in range(len(conditions_1)):
-            if locals()[f'count_{i}'] != 0:
-                name_class_1.append(conditions_1[i])
-                sizes_1.append(locals()[f'count_{i}'])
-                
-        fig, ax = plt.subplots(figsize = (6, 6))
-        ax.pie(sizes_1, autopct = '%1.1f%%' , startangle = 90)
-        ax.axis('equal')
-        if len(name_class_1) == 1:
-            plt.legend(name_class_1, prop={'size': 7}, loc='upper right')
-        elif len(name_class_1) == 2:
-            plt.legend(name_class_1, prop={'size': 6}, loc='upper right')
-        elif len(name_class_1) == 3:
-            plt.legend(name_class_1, prop={'size': 6}, loc='upper right')
-        elif len(name_class_1) == 4:
-            plt.legend(name_class_1, prop={'size': 6}, loc='upper right')
-        elif len(name_class_1) == 5:
-            plt.legend(name_class_1, prop={'size': 6}, loc='upper right')
-        plt.title('BIỂU ĐỒ BIỂU DIỄN SỰ SO SÁNH SỐ LƯỢNG HỌC SINH TỪNG LỚP HỌC')
-        st.pyplot(fig)
+            count_0 = len(df[df['CLASS'].str[2:4].isin(['CV'])])
+            count_1 = len(df[df['CLASS'].str[2:5].isin(['CT1', 'CT2', 'CT3'])])
+            count_2 = len(df[df['CLASS'].str[2:4].isin(['CL'])])
+            count_3 = len(df[df['CLASS'].str[2:4].isin(['CH'])])
+            count_4 = len(df[df['CLASS'].str[2:4].isin(['CA'])])
+            count_5 = len(df[df['CLASS'].str[2:6].isin(['CTIN'])])
+            count_6 = len(df[df['CLASS'].str[2:5].isin(['CSD'])])
+            count_7 = len(df[df['CLASS'].str[2:6].isin(['CTRN'])])
+            count_8 = len(df[df['CLASS'].str[2:4].isin(['TH', 'SN'])])
+            count_9 = len(df[df['CLASS'].str[2].isin(['A', 'B'])])
+
+            for i in range(len(conditions_1)):
+                if locals()[f'count_{i}'] != 0:
+                    name_class_1.append(conditions_1[i])
+                    sizes_1.append(locals()[f'count_{i}'])
+            
+            if len(sizes_1) == 1:
+                st.success(f'**Kết luận**:\n\n- Chỉ có duy nhất một loại đối tượng lớp học **{name_class_1[0]}** (100%).\n\n- Chứng tỏ các lớp học AI thu hút chủ yếu các học sinh của lớp **{name_class_1[0]}**.', icon = "✍️")
+            elif len(sizes_1) == 2:
+                if (sizes[0] < sizes[1]):
+                    st.success(f'**Kết luận**:\n\n- Có hai loại đối tượng lớp học: **{name_class_1[0]}** và **{name_class_1[1]}** .\n\n- Ta thấy số lượng học sinh lớp **{name_class_1[0]}** ít hơn số lượng học sinh lớp **{name_class_1[1]}**.\n\n- Chứng tỏ các lớp học AI thu hút học sinh học lớp **{name_class_1[1]}** nhiều hơn.', icon = "✍️")
+                elif (sizes[0] > sizes[1]):
+                    st.success(f'**Kết luận**:\n\n- Có hai loại đối tượng lớp học: **{name_class_1[0]}** và **{name_class_1[1]}** .\n\n- Ta thấy số lượng học sinh lớp **{name_class_1[1]}** ít hơn số lượng học sinh lớp **{name_class_1[0]}**.\n\n- Chứng tỏ các lớp học AI thu hút học sinh học lớp **{name_class_1[0]}** nhiều hơn.', icon = "✍️")
+                elif (sizes[0] == sizes[1]):
+                    st.success(f'**Kết luận**:\n\n- Có hai loại đối tượng lớp học: **{name_class_1[0]}** và **{name_class_1[1]}** .\n\n- Ta thấy số lượng học sinh lớp **{name_class_1[0]}** bằng số lượng học sinh lớp **{name_class_1[1]}**.\n\n- Chứng tỏ các lớp học AI thu  hút học sinh đều nhau.', icon = "✍️")
+            elif len(sizes_1) >= 3:
+                max_id = np.argmax(sizes_1)
+                min_id = np.argmin(sizes_1)
+                st.success(f'**Kết luận**:\n\n- Lớp có số lượng học sinh học đông nhất là: **{name_class_1[max_id]}** ({sizes_1[max_id]} học sinh).\n\n- Lớp học có số lượng học sinh học ít nhất là: **{name_class_1[min_id]}** ({sizes_1[min_id]} học sinh).\n\n- Chứng tỏ học sinh học lớp **{name_class_1[max_id]}** yêu thích môn AI hơn các lớp còn lại, trong khi đó các học sinh học lớp **{name_class_1[min_id]}** lại không thích AI hơn. Đồng thời, các lớp còn lại thì có số lưọng học sinh yêu thích các lớp AI bình thường.', icon = "✍️")
+            # Draw the pie chart 3
+            conditions_2 = {
+                'M': 'Nam',
+                'F': 'Nữ'
+            }
+            
+            df['GENDER'] = df['GENDER'].map(conditions_2)
+            
+            fig = px.pie(df, names = 'GENDER', title = 'BIỂU ĐỒ BIỂU DIỄN SỰ SO SÁNH SỐ LƯỢNG HỌC SINH NAM VÀ NỮ')
+            fig.update_layout(title_x=0.1, title_y=0.9)
+            st.plotly_chart(fig)
+            
+            if cnt_1 == 0:
+                st.success(f'**Kết luận**:\n\n- Chỉ có duy nhất một giới tính **Nữ** (100%).\n\n- Chứng tỏ các lớp học AI thu hút nhiều học sinh **Nữ**.', icon = "✍️")
+            elif cnt_2 == 0:
+                st.success(f'**Kết luận**:\n\n- Chỉ có duy nhất một giới tính **Nam** (100%).\n\n- Chứng tỏ các lớp học AI thu hút nhiều học sinh **Nam**.', icon = "✍️")
+            elif cnt_1 > cnt_2:
+                st.success(f'**Kết luận**:\n\n- Có hai giới tính **Nữ** và **Nam**.\n\n- Số lượng học sinh **Nam** nhiều hơn số lượng học sinh **Nữ**.\n\n- Chứng tỏ các lớp học AI thu hút số lượng học sinh **Nam** đông đảo hơn.', icon = "✍️")
+            elif cnt_1 < cnt_2:
+                st.success(f'**Kết luận**:\n\n- Có hai giới tính **Nữ** và **Nam**.\n\n- Số lượng học sinh **Nữ** nhiều hơn số lượng học sinh **Nam**.\n\n- Chứng tỏ các lớp học AI thu hút số lượng học sinh **Nữ** đông đảo hơn.', icon = "✍️")
+            elif cnt_1 == cnt_2:
+                st.success(f'**Kết luận**:\n\n- Có hai giới tính **Nữ** và **Nam**.\n\n- Số lượng học sinh **Nam** bằng số lượng học sinh **Nữ**.\n\n- Chứng tỏ các lớp học AI thu hút số lượng học sinh **Nam** và **Nữ** đồng đều.', icon = "✍️")
+        else:
+            st.warning('**The DataFrame is empty !!!**', icon = "⚠️")
+        
+    with tab_B:
+        if len(df) != 0:
+            
+            genre = st.radio("**Điểm từng thành phần**", ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "GPA"], horizontal = True)
+            
+            rank = []
+            for i in df.index:
+                c = df.loc[i, 'CLASS']
+                if c[2:4] == 'CV':
+                    rank.append('Văn')
+                elif c[2:5] == 'CT1' or c[2:5] == 'CT2' or c[2:5] == 'CT3':
+                    rank.append('Toán')
+                elif c[2:4] == 'CL':
+                    rank.append('Lý')
+                elif c[2:4] == 'CH':
+                    rank.append('Hóa')
+                elif c[2:4] == 'CA':
+                    rank.append('Anh')
+                elif c[2:6] == 'CTIN':
+                    rank.append('Tin')
+                elif c[2:5] == 'CSD':
+                    rank.append('Sử Địa')
+                elif c[2:6] == 'CTRN':
+                    rank.append('Trung Nhật')
+                elif c[2:4] == 'TH' or c[2:4] == 'SN':
+                    rank.append('TH / SN')
+                elif c[2] == 'A' or c[2] == 'B':
+                    rank.append('Khác')
+                    
+            df['CLASS-GROUP'] = rank
+            
+            for i in range(9):
+                if genre == f'S{i + 1}':
+                    fig = px.box(df, x = 'PYTHON-CLASS', y = f'S{i + 1}', color = 'GENDER', title = f'BIỂU ĐỒ BIỂU DIỄN SỰ SO SÁNH ĐIỂM SESSION {i + 1} GIỮA CÁC BUỔI HỌC')
+                    fig.update_layout(title_x=0.1, title_y=0.9)
+                    st.plotly_chart(fig)
+
+                    fig1 = px.box(df, x = 'CLASS-GROUP', y = f'S{i + 1}', title = f'BIỂU ĐỒ BIỂU ĐIỄN SỰ SO SÁNH ĐIỂM SESSION {i + 1} GIỮA CÁC HỌC SINH TRONG LỚP')
+                    fig1.update_layout(title_x=0.1, title_y=0.9)
+                    st.plotly_chart(fig1)
+                    
+            if genre == 'GPA':
+                fig = px.box(df, x = 'PYTHON-CLASS', y = 'GPA', color = 'GENDER', title = 'BIỂU ĐỒ BIỂU DIỄN SỰ SO SÁNH ĐIỂM GPA GIỮA CÁC BUỔI HỌC')
+                fig.update_layout(title_x=0.1, title_y=0.9)
+                st.plotly_chart(fig)
+
+                fig1 = px.box(df, x = 'CLASS-GROUP', y = 'GPA', title = 'BIỂU ĐỒ BIỂU ĐIỄN SỰ SO SÁNH ĐIỂM GPA GIỮA CÁC HỌC SINH TRONG LỚP')
+                fig1.update_layout(title_x=0.1, title_y=0.9)
+                st.plotly_chart(fig1)                
+
+        else:
+            st.warning('**The DataFrame is empty !!!**', icon = "⚠️")
